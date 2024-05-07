f1aef5583f4e7022fdf3d7c3f4390dea2156e584
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-05-07 11:07:58 +0300

    refactor: authenticate using clerk

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,26 +1,34 @@
-import { Montserrat as FontSans } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import type { Metadata } from "next";
 
 import { cn } from "@/lib/utils";
+import { ConfettiProvider } from "@/lib/providers/confetti-provider";
+
+import { Toaster } from "@/components/ui/toaster";
 
 import "@/app/styles/globals.css";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
-	title: {
-		default: "Next.JS Tailwind CSS Starter Template",
-		template: "%s | Superior Software Solutions",
-	},
-	description: "Starter template for Next.JS projects using Tailwind CSS.",
+	title: "Jifunze Learning System",
+	description:
+		"Jifunze: Expand your knowledge without breaking the bank.  Affordable, high-quality online courses on the skills you need.",
 };
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
 	return (
-		<html lang="en">
-			<body className={cn("font-sans antialiased", fontSans.variable)}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={cn("font-sans antialiased", fontSans.variable)}>
+					{children}
+					<Toaster />
+					<ConfettiProvider />
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 };
 
