5906e097d27ddc95f7094d35161c044bf59182ea
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-05-07 16:53:21 +0300

    refactor: build basic layout (#3)
    * refactor: center sign-in and sign-up forms
    
    * refactor: build basic layout

--- src/app/(app)/(auth)/(routes)/layout.tsx
+++ src/app/(app)/(auth)/(routes)/layout.tsx
@@ -1,5 +1,7 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-	return <main className="flex h-full items-center justify-center">{children}</main>;
+	return (
+		<main className="flex min-h-screen items-center justify-center">{children}</main>
+	);
 };
 
 export default AuthLayout;
