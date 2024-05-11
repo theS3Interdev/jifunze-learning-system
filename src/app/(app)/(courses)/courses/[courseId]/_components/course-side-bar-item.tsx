ef90b667cba5805dee7f34ab803ef60812d03b0b
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-05-11 15:25:42 +0300

    refactor: chapter progress functionality (#15)

--- src/app/(app)/(courses)/courses/[courseId]/_components/course-side-bar-item.tsx
+++ src/app/(app)/(courses)/courses/[courseId]/_components/course-side-bar-item.tsx
@@ -33,12 +33,12 @@ export const CourseSideBarItem = ({ label, id, isCompleted, courseId, isLocked }
 			className={cn(
 				"flex items-center gap-x-2 pl-6 text-sm font-[500] text-slate-500 transition-all hover:bg-sky-300/20 hover:text-sky-600",
 				isActive && "bg-slate-200/20 text-sky-700 hover:bg-sky-200/20 hover:text-sky-700",
-				isCompleted && "text-emerald-700 hover:text-emerald-700",
-				isCompleted && isActive && "bg-emerald-200/20",
+				isCompleted && "text-sky-700 hover:text-sky-700",
+				isCompleted && isActive && "bg-sky-200/20",
 			)}
 		>
 			<div className="flex items-center gap-x-2 py-4">
-				<Icon size={22} className={cn("text-slate-500", isActive && "text-sky-700", isCompleted && "text-emerald-700")} />
+				<Icon size={22} className={cn("text-slate-500", isActive && "text-sky-700", isCompleted && "text-sky-700")} />
 				{label}
 			</div>
 
@@ -46,7 +46,7 @@ export const CourseSideBarItem = ({ label, id, isCompleted, courseId, isLocked }
 				className={cn(
 					"ml-auto h-full border-2 border-primary opacity-0 transition-all",
 					isActive && "opacity-100",
-					isCompleted && "border-emerald-700",
+					isCompleted && "border-sky-700",
 				)}
 			/>
 		</button>
